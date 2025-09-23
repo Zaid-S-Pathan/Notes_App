403dc14c4cbd8123876f268c8018ff0db54d2456
Zaid-S-Pathan <mohammedzaid.s.pathan@gmail.com>
2025-09-24 01:06:11 +0530

    setting changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -157,7 +157,10 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://notes-app-virid-delta.vercel.app",
+    "http://localhost:3000",  # For local development
+]
 CORS_ALLOW_CREDENTIALS = True
 
 # Production settings
